7752b1efda88a47c4b398f41b2824d11ccf94db9
eliapts <39885911+eliapts@users.noreply.github.com>
2019-09-30 21:58:02 +0400

    Create Main.java

---
+++ Homework1-2/Main.java
@@ -0,0 +1,18 @@
+package BTU;
+
+public class Main {
+
+    public static void main(String[] args) {
+	    A myproject = new A();
+	    myproject.InputMethod();
+	    myproject.SumMethod();
+	    myproject.EvenOrOdd();
+
+	    B project = new B();
+	    project.inputMethod();
+        project.InputMethod();
+
+        System.out.println(project.Sum());
+
+    }
+}
