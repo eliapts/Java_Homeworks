fc49b02cf1e1fc09b9968bff966896d38638e1bb
eliapts <39885911+eliapts@users.noreply.github.com>
2019-09-30 15:42:53 +0400

    Create Main.java

---
+++ Homework1/Main.java
@@ -0,0 +1,15 @@
+package BTU;
+
+public class Main {
+
+    public static void main(String[] args) {
+	C myProj = new C();
+	myProj.first();
+	System.out.println(myProj.lastDigit());
+	System.out.println(myProj.firstDigit());
+	System.out.println(myProj.sumMethod());
+	myProj.mult();
+	myProj.Sum();
+
+    }
+}
