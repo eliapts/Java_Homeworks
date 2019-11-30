64657dbe75fe1a8197d887b37a61f704d34da444
eliapts <39885911+eliapts@users.noreply.github.com>
2019-11-30 18:59:35 +0400

    Create GeneratorInterface.java

---
+++ Homework4/GeneratorInterface.java
@@ -0,0 +1,8 @@
+package generator;
+
+public interface GeneratorInterface {
+    public void generateAlphabet();
+    public char generateRandomChar();
+    public String generateRandomWord();
+    public String generateSentence();
+}
